d48cbb0868cdf7b2e9598f71b580f214be8465a1
Dima <vectorforce26@mail.ru>
2020-05-07 03:19:05 +0300

    Wrote script for auto start of compiler with user file as parameter.

---
+++ lmocc.bat
@@ -0,0 +1,4 @@
+javac -sourcepath ./src -d bin src/main/java/com/vectorforce/compiler/Compiler.java %1
+java -classpath ./bin main.java.com.vectorforce.compiler.Compiler
+javac Main.java
+java Main
--- src/main/java/com/vectorforce/compiler/Compiler.java
+++ src/main/java/com/vectorforce/compiler/Compiler.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class Compiler {
     public static void main(String[] args) throws IOException {
-        final String inputText1 = new String(Files.readAllBytes(Paths.get("Test program1.lmoc")), "UTF-8");
+        final String inputText1 = new String(Files.readAllBytes(Paths.get(args[0])), "UTF-8");
         final List<Token> tokens = new LexicalAnalyzer(inputText1).tokenize();
         final Statement statement = new Parser(tokens).parse();
         statement.execute();
